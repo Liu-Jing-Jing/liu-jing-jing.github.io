d906789d52640d83f4004bf183e71495becf0531
marklewis <marklewis@qq.com>
2020-03-07 16:23:19 +0800

    Clean workplace

--- Mark_bo_ke_zhan_dian/Blog/gui_dang_files/gui_dang.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20180204
-
-setTransparentGifURL('../Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('gui_dang_files/gui_dangMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../Media/transparent.gif');fixupIECSS3Opacity('id3');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
