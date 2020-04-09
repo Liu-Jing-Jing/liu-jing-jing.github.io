4a3001a4aa649bd519ec147b61fea3c60d021afd
Mark <marklewis@qq.com>
2020-04-09 09:56:13 +0800

    add animation for low animation

--- common.js
+++ common.js
@@ -60,3 +60,112 @@ function getPreviousElement(elm) {
     // 返回数组
     return arr;
 }
+
+//======================================animation
+/**
+ * Created by liujing on 20-4-3.
+ */
+/**
+ *
+ * @param {position} elm
+ * @param {目标位置} target
+ */
+function moveAnimation(elm,target) {
+    clearInterval(elm.timeID);
+    var position = elm.offsetLeft;
+    var step = (target - position) > 0 ? 10 : -10;
+    elm.timeID = setInterval(
+        function () {
+            position += step;
+            if (Math.abs(target - position) > Math.abs(step)) {
+                elm.style.left = position + 'px';
+            } else {
+                elm.style.left = target + 'px';
+                clearInterval(elm.timeID);
+            }
+        },20
+    )
+}
+
+function getStyle(box, styleString)
+{
+    return window.getComputedStyle(box, null)[styleString];
+}
+
+
+
+//
+var box = document.getElementById('box');
+var btn = document.getElementById('btn');
+
+/* 回调函数版本的 Block*/
+function slowAnimationWithBlock(ele, obj, fun)
+{
+    clearInterval(ele.timeID);
+
+    ele.timeID = setInterval(
+        function () {
+            var flag = true;
+
+            for(var key in obj){
+                var position = window.getComputedStyle(ele)[key];
+                position = parseInt(position);
+
+                // obj key  以前的target
+                var step = (obj[key] - position) > 0? Math.ceil(obj[key]- position/10):  Math.floor(obj[key] - position)/10;
+                position += step;
+                ele.style[key] = position + 'px';
+
+                if(position != obj[key])
+                {
+                    flag = false;
+                }
+            }
+
+            if(flag)
+            {
+                clearInterval(ele.timeID);
+                ///**/
+                /*optc SHORTCUT*/
+                if(Object.prototype.toString.call(fun) == '[object Function]');
+            }
+        },
+        20
+    );
+
+}
+
+
+function slowAnimationWithMultiArgs(ele, obj) {
+    clearInterval(ele.timeID);
+
+    ele.timeID = setInterval(
+        function () {
+            //假设一个条件
+            var flag = true;
+
+            for (var key in obj){
+                var position = window.getComputedStyle(ele)[key];
+
+                position = parseInt(position);
+
+                var step = (obj[key] - position) > 0? Math.ceil(obj[key]- position/10):  Math.floor(obj[key] - position)/10;
+                position += step;
+                ele.style[key] = position + 'px';
+
+                if(position != obj[key])
+                {
+                    flag = false;
+                }
+            }
+
+
+            if(flag)
+            {
+                clearInterval(ele.timeID);
+                //
+            }
+        },
+        20
+    );
+}
