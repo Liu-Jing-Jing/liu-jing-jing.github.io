af19929eb9fe15e9cd471c746fa7aa863ab1ec7a
Mark <marklewis@qq.com>
2020-04-09 09:54:38 +0800

    Update common.js

--- common.js
+++ common.js
@@ -1,6 +1,25 @@
 // 自己封装的函数工具包
 // 封装代码的目的就是为了节省写代码的时间
-
+/**
+ * 获得页面滚动距离的兼容代码
+ * 返回一个对象,拥有水平与垂直的距离
+ */
+function getScroll() {
+    return {
+        scrollTop: document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop ||0,
+        scrollLeft: document.documentElement.scrollLeft || window.pageXOffset || document.body.scrollLeft ||0
+    }
+}
+/**
+ * 获得页面的可视区域
+ * 返回一个对象
+ */
+function getClient() {
+    return {
+        clientWidth :  document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth || 0,
+        clientHeight :  document.documentElement.clientHeight || document.body.clientHeight || window.innerHeight || 0
+    }
+}
 
 /**
  * 获得当前元素后面的所有元素节点
