f4de4f66e1a39da7254dfd41dbf3e06fdaceedd6
Mark <marklewis@qq.com>
2020-04-11 16:26:55 +0800

    Update common.js

--- common.js
+++ common.js
@@ -39,6 +39,54 @@ function setInnerText(element, content) {
     element.textContent = content;
   }
 }
+
+
+// 处理注册事件的兼容性问题
+// eventName, 不带on，  click  mouseover  mouseout
+function addEventListener(element, eventName, fn) {
+  // 判断当前浏览器是否支持addEventListener 方法
+  if (element.addEventListener) {
+    element.addEventListener(eventName, fn);  // 第三个参数 默认是false
+  } else if (element.attachEvent) {
+    element.attachEvent('on' + eventName, fn);
+  } else {
+    // 相当于 element.onclick = fn;
+    element['on' + eventName] = fn;
+  }
+}
+
+// 处理移除事件的兼容性处理
+function removeEventListener(element, eventName, fn) {
+  if (element.removeEventListener) {
+    element.removeEventListener(eventName, fn);
+  } else if (element.detachEvent) {
+    element.detachEvent('on' + eventName, fn);
+  } else {
+    element['on' + eventName] = null;
+  }
+}
+
+// 获取页面滚动距离的浏览器兼容性问题
+// 获取页面滚动出去的距离
+function getScroll() {
+  var scrollLeft = document.body.scrollLeft || document.documentElement.scrollLeft;
+  var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+  return {
+    scrollLeft: scrollLeft,
+    scrollTop: scrollTop
+  }
+}
+
+// 获取鼠标在页面的位置，处理浏览器兼容性
+function getPage(e) {
+  var pageX = e.pageX || e.clientX + getScroll().scrollLeft;
+  var pageY = e.pageY || e.clientY + getScroll().scrollTop;
+  return {
+    pageX: pageX,
+    pageY: pageY
+  }
+}
+
 /**
  * 获得页面滚动距离的兼容代码
  * 返回一个对象,拥有水平与垂直的距离
