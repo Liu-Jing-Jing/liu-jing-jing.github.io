bd04f2071b335c0696478066e7c985d386561cf9
Mark <marklewis@qq.com>
2020-05-09 00:00:21 +0800

    Create jQuery140.js

---
+++ js/jQuery140.js
@@ -0,0 +1,15 @@
+/*!
+ * jQuery JavaScript Library v1.4
+ * http://jquery.com/
+ *
+ * Copyright 2010, John Resig
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://docs.jquery.com/License
+ *
+ * Includes Sizzle.js
+ * http://sizzlejs.com/
+ * Copyright 2010, The Dojo Foundation
+ * Released under the MIT, BSD, and GPL Licenses.
+ *
+ * Date: Wed Jan 13 15:23:05 2010 -0500
+ */
