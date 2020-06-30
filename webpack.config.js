969e8895cc2a28cb37b45b218b14956b1a3da011
Mark <marklewis@qq.com>
2020-06-30 08:47:07 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,25 @@
-webpack cli的使用
+//webpack cli的使用
+
+module.exports = {
+    entry: './src/main.js',
+
+    // 设置项目的模式development production两种
+    mode: 'development',
+    module: {
+        rules:[
+            {
+                test:/\.css$/,
+                use:[
+                    'style-loader',
+                    'css-loader', //要特别注意 先倒入style-loader 然后才导入css-loader
+                ],
+                test:/\.less$/,
+                use:[
+                    'style-loader',
+                    'css-loader', //要特别注意 先倒入style-loader 然后才导入css-loader
+                    'less-loader'
+                ],
+            }
+        ],
+      },
+}
