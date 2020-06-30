f57c35db9269c0538748eaeccd300da51582e0c8
Mark <marklewis@qq.com>
2020-06-30 11:18:54 +0800

    Webpack基本的使用

--- webpack.config.js
+++ webpack.config.js
@@ -28,3 +28,10 @@ module.exports = {
 // 了解webpack的插件
 // 使用html-webpack-plugin 
 //插件 在module.exports = {} 里面配置plugin数组
+
+/**
+    webpack的各种插件cleanWebpackPlugin 用来在你每次打包之前清空dist目录
+    webpack-dev-serve 是启动服务器的插件在devServer:{}配置contentBase静态文件路径
+    设置hot:true的时候能实现vue的脚手架效果
+    npm run serve命令就是利用了webpack-dev-serve插件实现的
+*/
